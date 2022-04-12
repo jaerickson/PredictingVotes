10613062499a9c8b896a8db118771d7ff4a4c417
DoctorEtc <100000551+DoctorEtc@users.noreply.github.com>
2022-04-12 15:53:11 -0700

    Update votes.py

--- votes.py
+++ votes.py
@@ -9,6 +9,9 @@ data = pd.read_csv('condensed_cvr.csv', dtype=str).sample(n=20000, random_state=
 # Set aside some data for testing
 # TODO You have to do this, using train_test_split. Put 20% of the data into the test set.
 # Use 42 as the random number seed.
+#Splits data into a training set and a test set.
+# Test set = 20%
+# Train Set = 80%
 train_set, test_set = train_test_split(data,test_size=0.2,train_size=0.8,random_state=42)
 
 # Separate the target column as the label for supervised learning
@@ -22,9 +25,14 @@ train_y = train_set[TARGET_COLUMN].copy()
 # TODO You have to do this. When creating your OneHotEncoder, specify handle_unknown='ignore', as there
 # may be votes that appear in the test set but not in the training set. Call the augmented train_X
 # "train_X_1hot".
+#Simplifies the input data into a format that can be more easily read categorically by the computer
+vote_encoder = OneHotEncoder(handle_unknown='ignore')
+train_X_1hot = vote_encoder.fit_transform(train_X)
 
 # Train a decision tree
 # TODO You have to do this. Use a max depth of 6. Call your tree "tree".
+tree = DecisionTreeClassifier(max_depth = 6) #Use DecisionTreeClassifier
+tree.fit(train_X_1hot,train_y) #Fit data to tree
 
 # Test it on the training set
 print(f'Accuracy on training set: {tree.score(train_X_1hot, train_y)}')
